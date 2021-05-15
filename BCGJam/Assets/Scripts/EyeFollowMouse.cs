cd64e289c5c8e7e6e64972e1d6e52ec29adbde5a
Team-on <timonsol13@gmail.com>
2021-05-15 23:41:50 +0300

    New sprites

---
+++ BCGJam/Assets/Scripts/EyeFollowMouse.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+using NaughtyAttributes;
+using Random = UnityEngine.Random;
+
+public class EyeFollowMouse : MonoBehaviour {
+	[Header("Data"), Space]
+	[SerializeField] Transform center;
+	[SerializeField] float radius = 6.0f;
+
+	void Update() {
+		Vector3 mousePosScreen = Mouse.current.position.ReadValue();
+		//Vector3 mousePosWorld = TemplateGameManager.Instance.Camera.ScreenToWorldPoint(mousePosLocal).SetZ(0.0f);
+		Vector3 offset = mousePosScreen - center.transform.position;
+
+		transform.position = center.position + offset.normalized * Mathf.Lerp(0, radius, offset.magnitude / 500f);
+		transform.eulerAngles = Vector3.zero;
+	}
+}
